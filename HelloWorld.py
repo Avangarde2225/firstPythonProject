8e6cac34b0ad09900f9ff8c3eff2a0e1216a553b
Serdar <serdar2225@gmail.com>
2020-10-16 11:53:36 -0400

    python examples 1

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+print('Hello World')
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's Strings are easy to use")
+print('We can include "quotes" in the Strings')
+print("hello" + " 7")
