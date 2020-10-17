7fe256cab7ef6a1e8104205c35c98adfcd927cde
Serdar <serdar2225@gmail.com>
2020-10-17 07:59:04 -0400

    python examples 6

---
+++ string_operators.py
@@ -0,0 +1,20 @@
+string1 = "He's "
+string2 = "probably "
+string3= "pining "
+string4= "in the "
+string5 = "fjords."
+
+print(string1 + string2 + string3 + string4 + string5)
+
+print("He's " "probably " "pining " "in the " "fjords.")
+
+print("hello " * 5)
+print("hello " * 5 + " 4")
+
+today = "friday"
+
+print("day" in today)
+print("fri" in today)
+print("thu" in today)
+print("carrot" in "fjords")
+
