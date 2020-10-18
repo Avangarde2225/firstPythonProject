7fff6302b44a079917bcf90f79d2c6313b137114
Serdar <serdar2225@gmail.com>
2020-10-18 17:39:24 -0400

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def is_palindrome(string):
+    return string[::-1] == string
+    # backwards = string[::-1]
+    # return backwards == string
+
+
+
+word = input("Please enter a word to reverse: ")
+if is_palindrome(word):
+    print("{} is a palindrome".format(word))
+else:
+    print("{} is not a palindrome".format(word))
+
