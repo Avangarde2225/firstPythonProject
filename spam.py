a2ffb63e7c4fa9e27b868e02eea8a45ef9a65197
Serdar <serdar2225@gmail.com>
2020-12-14 00:50:36 -0500

    formatting

--- spam.py
+++ spam.py
@@ -12,3 +12,8 @@ menu = [
 for meal in menu:
     if "spam" not in meal:
         print(meal)
+        for item in meal:
+            print(item)
+    else:
+        print("{0}has a spam score of {1}"
+              .format(meal, meal.count("spam")))
