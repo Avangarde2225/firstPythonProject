bd327bca1a607cf2bfaa030f61a2b3873b6d376d
Serdar <serdar2225@gmail.com>
2020-12-14 01:26:54 -0500

    no spam

--- nospam.py
+++ nospam.py
@@ -10,10 +10,7 @@ menu = [
 ]
 
 for meal in menu:
-    if "spam" not in meal:
-        print(meal)
-        for item in meal:
-            print(item)
-    else:
-        print("{0} has a spam score of {1}"
-              .format(meal, meal.count("spam")))
+    for index in range(len(meal) - 1, -1, -1):
+        if meal[index] == "spam":
+            del meal[index]
+    print(meal)
