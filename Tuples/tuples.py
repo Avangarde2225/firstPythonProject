2d03c9947593bcd5c225f27c7caf94b94e25b727
Serdar <serdar2225@gmail.com>
2020-12-25 18:38:56 -0500

    tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,10 @@
+#you can define tuples with or without ()
+t = "a" , "b", "c"
+print(t)
+
+#() are needed
+name = "Time"
+age = 10
+
+print(name, age, "Python",2020)
+print((name, age, "Python",2020))
--- split_method.py
+++ split_method.py
@@ -1,3 +1,12 @@
 panagram = " The quick fox jumps over the lazy dog"
 
-print(panagram.split())
+words = panagram.split()
+print(words)
+
+
+
+numbers = "9,223,372,036,854,775,807"
+print(numbers.split(','))
+
+#challenge
+
