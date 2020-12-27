d4afa311b9ee17b819c4172687bda6501b8d47b8
Serdar <serdar2225@gmail.com>
2020-12-26 21:25:25 -0500

    palindrome some adjustments

---
+++ functions/functions_intro.py
@@ -0,0 +1,7 @@
+def multiply(x,y):
+    result = x * y
+    return result
+
+
+answer = multiply(10.5, 4)
+print(answer)
--- palindrome.py
+++ palindrome.py
@@ -4,10 +4,23 @@ def is_palindrome(string):
     # return backwards == string
 
 
+def  palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string +=char
+
+
+    return string[::-1].casefold() == string.casefold()
+
+
 
 word = input("Please enter a word to reverse: ")
-if is_palindrome(word.casefold()):
+if palindrome_sentence(word):
+#if is_palindrome(word.casefold()):
     print("{} is a palindrome".format(word))
 else:
     print("{} is not a palindrome".format(word))
+
+
 
