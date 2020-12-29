1f168567f03982fa583db0b511c276e49e383c68
Serdar <serdar2225@gmail.com>
2020-12-28 23:50:43 -0500

    dictionaries

---
+++ dictionaries/dictionaries.py
@@ -0,0 +1,22 @@
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit"}
+
+print(fruit)
+print(fruit["lemon"])
+fruit["pear"] = "that bears love"
+print(fruit)
+del fruit["lemon"]
+print(fruit)
+
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        print("We dont have a " + dict_key)
