210dd970adec52923dcb4a0af29ce55bdce65c06
Serdar <serdar2225@gmail.com>
2020-12-29 00:50:11 -0500

    sorted dics

---
+++ dictionaries/dictionariesSorted.py
@@ -0,0 +1,13 @@
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit"}
+
+# ordered_keys = list(fruit.keys())
+# print(ordered_keys)
+
+ordered_keys = sorted(list(fruit.keys()))
+for f in ordered_keys:
+    print(f + " - " + fruit[f] )
+
