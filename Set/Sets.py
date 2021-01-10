01b16a4ba6b86bb363907b60758ce5bf9af65bbc
Serdar <serdar2225@gmail.com>
2021-01-09 21:48:44 -0500

    dics and sets

---
+++ Set/Sets.py
@@ -0,0 +1,10 @@
+farm_animals = {"sheep", "cow" , "hen"}
+print(farm_animals)
+
+for animal in farm_animals:
+    print(animal)
+
+print("=" * 40)
+
+wild_animals = set(["lion", "tiger", "panter", "elephant"])
+print(wild_animals)
